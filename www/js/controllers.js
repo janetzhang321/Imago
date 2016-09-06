8bdebf0e0b0879d6daca35d3520c9928696dbd37
Janet Zhang <janetzhang321@gmail.com>
2016-09-06 03:12:05 -0400

    got bucketlists to redirect

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -412,10 +412,10 @@ $state.go('detail',{imagoName:currentImago});
   })
 
 
-  .controller('buildingsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+  .controller('buildingsCtrl', ['$scope', '$stateParams', 'ImagoFactory',// The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
     // You can include any angular dependencies as parameters for this function
     // TIP: Access Route Parameters for your page via $stateParams.parameterName
-    function ($scope, $stateParams) {
+    function ($scope, $stateParams, ImagoFactory) {
 
 
       var markers = ImagoFactory.markers;
@@ -452,11 +452,10 @@ $state.go('detail',{imagoName:currentImago});
 
     }])
 
-  .controller('businessesCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+  .controller('businessesCtrl', ['$scope', '$stateParams', 'ImagoFactory',// The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
     // You can include any angular dependencies as parameters for this function
     // TIP: Access Route Parameters for your page via $stateParams.parameterName
-    function ($scope, $stateParams) {
-
+    function ($scope, $stateParams, ImagoFactory) {
 
       var markers = ImagoFactory.markers;
 
